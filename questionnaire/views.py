d5e23949c2cbdc155c519ce5efaca3f6ae0671c8
alexum <alexum2013@yandex.ru>
2022-03-26 21:39:36 +0300

    add questionnaire list view

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Questionnaires
+
 
 # Create your views here.
+
+class QuestionnaireList(ListView):
+    model = Questionnaires
+    template_name = 'questionnaires/questionnaire_list.html'
